1dc7fd56ab6a696b534093a1be521c49ba0a0559
Priscilla Infante <U91607@ust-global.com>
2019-01-28 14:55:03 -0600

    js

--- webservices.js
+++ webservices.js
@@ -5,4 +5,26 @@ fetchData();
 
 function fetchData(){
 
+    var request = new XMLHttpRequest();
+  request.open('GET', URL, true);
+
+  request.onload = function() {
+    const OK = 200;
+
+    if (request.status !== OK) {
+        document.innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
+        return;
+      }
+
+      var remoteStudents = JSON.parse(request.responseText);
+
+      remoteStudents.forEach(element => {
+       students.push(element);
+      });
+     loadDataGrid();
+
+    };
+
+   request.send();
+
 } 
